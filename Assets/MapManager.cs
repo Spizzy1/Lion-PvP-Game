78dcf67b7f8613b681fced837c050e9d27841f62
Spizzy <spizzy47spizzy@gmail.com>
2022-12-13 10:42:46 +0100

    Started working on map change system

---
+++ Assets/MapManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] maps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void loadMap() //selects a random map to load in when progressing to next stage
+    {
+        int randomIndex = Random.Range(0, maps.Length);
+        float winPlayerX = GameObject.FindGameObjectWithTag("Player").transform.position.x;
+        GameObject loadedMap = Instantiate(maps[randomIndex], new Vector3(winPlayerX/Mathf.Abs(winPlayerX), 0, 0), Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
