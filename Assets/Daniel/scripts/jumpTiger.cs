1efff75e1ba2ee636a234493fccbf4bdcacfb73e
NEOBENGUIN <daniel.wan@elev.ga.lbs.se>
2022-12-14 15:33:23 +0100

    I MADE JUMP AND DIE ON TOUCH OF PERSIH PIT

--- Assets/Daniel/scripts/jumpTiger.cs
+++ Assets/Daniel/scripts/jumpTiger.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class jumpTiger : MonoBehaviour
 {
-    [SerializeField] private float jumpForce = 10;
+    [SerializeField] private float jumpForce = 4;
     // Start is called before the first frame update
     private Rigidbody2D rb;
+   bool isGrounded = true;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
+    void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.GetComponent<groundedforever>()){
+            isGrounded = true;
+        }
+    }
+  
+    void OnCollisionExit(Collision collision){
+        if (collision.gameObject.GetComponent<groundedforever>())
+        {
+            isGrounded = false;
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("perishPit"))
+        {
+            Destroy(gameObject);
+        }
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            if (isGrounded == true)
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
         }
     }
 }
